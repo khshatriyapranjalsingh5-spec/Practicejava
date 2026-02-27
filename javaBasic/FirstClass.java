ebcd38a0cdbfd3bcf57764daff0999d9718fca99
PranjalSingh <khshatriyapranjalsingh5@gmail.com>
2026-02-27 14:35:17 +0530

    first comment

---
+++ javaBasic/FirstClass.java
@@ -0,0 +1,8 @@
+package javaBasic;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+         System.out.println("Hello 2");
+    }
+}
