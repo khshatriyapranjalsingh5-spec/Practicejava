f1a058c2f89b13bd81acdaa301f9d27c0df0cd96
Pranjal Singh <khshatriyapranjalsingh5@gmail.com>
2026-03-23 23:31:27 +0530

    2nd file added

---
+++ Conditionalstatment/GreatestOfNumber.java
@@ -0,0 +1,32 @@
+package Conditionalstatment;
+
+import java.util.Scanner;
+
+public class GreatestOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter Second number : ");
+        int b = sc.nextInt();
+        System.out.println("Enter Third number : ");
+        int c = sc.nextInt();
+        if (a > b) {
+            if (a > c) {
+                System.out.println("(a) First number is greatest. ");
+            }
+            else{
+                System.out.println("(c) Third number is greatest. ");
+            }
+        }
+        else{
+            if (b > c) {
+                System.out.println("(b) Second number is greatest. ");
+            }
+            else{
+                System.out.println("(c) Third number is greatest. ");
+            }
+        }
+    }
+    
+}
