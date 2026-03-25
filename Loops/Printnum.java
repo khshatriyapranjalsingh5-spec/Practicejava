8ad4c59f76008c18b4a238ddd92c093c8cef66e6
Pranjal Singh <khshatriyapranjalsingh5@gmail.com>
2026-03-25 10:32:08 +0530

    loops first code

---
+++ Loops/Printnum.java
@@ -0,0 +1,10 @@
+package Loops;
+
+public class Printnum {
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
