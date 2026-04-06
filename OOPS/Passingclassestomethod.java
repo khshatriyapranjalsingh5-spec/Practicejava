254e4225f71582c149ebcd6a1b6ea565bf7569d6
Pranjal Singh <khshatriyapranjalsingh5@gmail.com>
2026-04-06 23:36:30 +0530

    oops second file

--- OOPS/Passingclassestomethod.java
+++ OOPS/Passingclassestomethod.java
@@ -8,18 +8,24 @@ public class Passingclassestomethod {
         double length;
         String type;
         int torque;
+        void print(){
+            System.out.println(name+" "+length+" "+seat+" "+type);  //method
+        }
     }
     public static void main(String[] args) {
         Car c = new Car();
         c.name = "Suv";
         c.length = 2.7;
         c.seat = 5;
+        c.type = "fourwheeler";
 
         change(c);
 
         System.out.println(c.seat);
+
+        c.print();
     }
-    private static void change(Car c){
+    private static void change(Car c){ //main file mein change
         c.seat = 4;
     }
 }
