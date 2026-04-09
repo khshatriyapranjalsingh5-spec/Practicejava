1ba3450ac31d972e19dcdb2109560aa389cc010e
Pranjal Singh <khshatriyapranjalsingh5@gmail.com>
2026-04-09 23:03:59 +0530

    4th file

--- OOPS/Privatekeyword.java
+++ OOPS/Privatekeyword.java
@@ -4,18 +4,26 @@ class Student{
         String name;
         private int rno = 28;
         double cgpa;
-        void print(){
+        void print(){   //getter
             System.out.println(name +" "+ rno +" "+ cgpa);
+        }
+        int getRno(){    //getter
+            return rno;
+        }
+        void setRno(int x){      //setter
+            if (rno > 0) {
+                rno = x ;
+            }
         }
 } 
 
 public class Privatekeyword {
     public static void main (String[] args){
-           Student s = new Student();
-           s.cgpa = 7.5 ; 
-           //s.rno = 96 ;
-           s.name ="Rohit";
-          // System.out.println(s.rno);
-           s.print();
+        Student s = new Student();
+        s.cgpa = 7.5 ; 
+        //s.rno = 96 ;
+        s.name ="Rohit";
+        s.setRno(45);
+        System.out.println(s.getRno());
     }
 }
