0bd13ff48b68e81ebb7c900e79b269a683eeda03
Pranjal Singh <khshatriyapranjalsingh5@gmail.com>
2026-04-19 01:06:24 +0530

    updated file

--- OOPS/finalandStatic.java
+++ OOPS/finalandStatic.java
@@ -10,13 +10,22 @@ class Cricketer{
         this.run = r;
         this.century = c;
     }
+    Cricketer(){
+        System.out.println(this.name+" "+this.run+" "+this.century);
+    }
     void print(){
         System.out.println(this.name+" "+this.run+" "+this.century);
+    }
+    static void greet(){
+        System.out.println("I Only believe in jassi bhai ");
     }
 }
 public class finalandStatic {
     public static void main(String[] args) {
-        Cricketer c1 =new Cricketer("Rohit",12550,50);
+        Cricketer c1 = new Cricketer("Rohit",12550,50);
         c1.print();
+        //Cricketer c2 = new Cricketer("Virat",15000,66);
+        Cricketer c2 = new Cricketer();
+        c2.greet();
     }
 }
